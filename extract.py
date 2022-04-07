b959d5d2b42926bebb50510ce850021d8099ce40
yxj <564197835@qq.com>
2022-04-07 16:14:05 +0800

    refactor

--- extract.py
+++ extract.py
@@ -13,18 +13,19 @@ def ctrl_lim(num):
     return num
 
 
+# pic_matrix 0-255的灰度图像
 def convolve(pic_matrix, new_matrix, kernel):
-    kernel_height = kernel.shape[0]
-    kernel_width = kernel.shape[1]
-    picture_height = pic_matrix.shape[0]
-    picture_width = pic_matrix.shape[1]
-    r = int((kernel.shape[0] - 1) / 2)
+    print((pic_matrix < 0).sum())
+    print(pic_matrix.dtype)
+    kernel_height, kernel_width = kernel.shape
+    picture_height, picture_width = pic_matrix.shape
+    r = int((kernel_height - 1) / 2)  # 找到中心位置
     for j in range(picture_height - kernel_height + 1):
         if j % 20 == 0:
             print("正在执行第" + str(j) + "行像素点")
         for i in range(picture_width - kernel_width + 1):
-            raw_color = pic_matrix[j + r][i + r]
-            # 卷积
+            raw_color = pic_matrix[j + r][i + r]  # 这个kernel中心的颜色
+            # 卷积 在当前的这个kernel中寻找最大的灰度值
             max_color = 0
             for m in range(kernel_height):
                 for n in range(kernel_width):
@@ -33,25 +34,23 @@ def convolve(pic_matrix, new_matrix, kernel):
             # 反色
             max_color = ctrl_lim(255 - max_color)
             # 颜色减淡
-            if raw_color == 255:
+            if raw_color == 255:  # 原来是白的现在还是白的
                 new_color = 255
-            elif max_color == 0:
+            elif max_color == 0:  # kernel内部有白色值 则颜色不变
                 new_color = raw_color
             else:
-                new_color = raw_color / (1 - max_color / 255)
+                new_color = raw_color / (
+                    1 - max_color / 255
+                )  # 255 * raw_color / max_color
 
             new_matrix[j][i] = ctrl_lim(new_color)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--pic_path", "-i", type=str, help="path of original picture")
+    parser.add_argument("--pic_path", "-i", type=str, help="输入图像的路径")
     parser.add_argument(
-        "--output_folder_path",
-        "-o",
-        type=str,
-        default="result",
-        help="path of output folder",
+        "--output_folder_path", "-o", type=str, default="result", help="输出文件夹的路径",
     )
     args = parser.parse_args()
 
@@ -60,15 +59,12 @@ if __name__ == "__main__":
     # translate a color image to greyscale: L = R * 299/1000 + G * 587/1000 + B * 114/1000
     pic = Image.open(pic_path).convert(mode="L")
     width, height = pic.size
+    print(pic.size)
     pic_matrix = np.array(pic).reshape((height, width))
 
-    lineart = Image.new("L", (width - 2, height - 2), 255)
-    lineart_matrix = np.array(lineart).reshape((height - 2, width - 2))
-
-    print("开始")
     kernel = np.zeros((5, 5))
+    lineart_matrix = np.zeros(shape=(height - 2, width - 2), dtype=np.int8)
     convolve(pic_matrix, lineart_matrix, kernel)
-    print("完毕")
-
     lineart = Image.fromarray(lineart_matrix, "L")
     lineart.save(Path(args.output_folder_path, pic_name))
+    lineart.show()
