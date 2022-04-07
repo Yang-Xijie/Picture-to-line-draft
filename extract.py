a74bb4e8a0b2f2533c4855efb61fac0f2e000882
yxj <564197835@qq.com>
2022-04-07 16:52:56 +0800

    use funtion to return matrix

--- extract.py
+++ extract.py
@@ -3,24 +3,27 @@ import numpy as np
 from PIL import Image  # https://pillow.readthedocs.io/en/stable/reference/Image.html
 from pathlib import Path
 
-
+# [param]
 # pic_matrix 0-255的灰度图像 numpy数组 类型np.uint8
 # kernel_size int 表示一个运算核的大小
-# new_matrix 0-255的灰度图像 numpy数组 类型np.uint8
-def convolve(pic_matrix, new_matrix, kernel_size=5):
+# [return]
+# lineart_matrix 0-255的灰度图像 numpy数组 类型np.uint8
+def convolve(pic_matrix, kernel_size=5):
     picture_height, picture_width = pic_matrix.shape
-    result_height = picture_height - kernel_size + 1
-    result_width = picture_width - kernel_size + 1
+    lineart_height = picture_height - kernel_size + 1
+    lineart_width = picture_width - kernel_size + 1
     mid_offset = int(kernel_size / 2)  # 中心位置的偏差
 
-    for r in range(result_height):
+    lineart_matrix = np.zeros(shape=(lineart_height, lineart_width), dtype=np.int8)
+    for r in range(lineart_height):
         print(r)
-        for c in range(result_width):
+        for c in range(lineart_width):
             raw_color = pic_matrix[r + mid_offset][c + mid_offset]  # 这个kernel中心的颜色
-            max_color = np.max(pic_matrix[r:(r+kernel_size),c:(c+kernel_size)])
+            max_color = np.max(pic_matrix[r : (r + kernel_size), c : (c + kernel_size)])
             # 颜色减淡: raw_color 与 max_color 越接近 new_color越白
             new_color = 255 * raw_color / max_color
-            new_matrix[r][c] = new_color
+            lineart_matrix[r][c] = new_color
+    return lineart_matrix
 
 
 if __name__ == "__main__":
@@ -39,9 +42,7 @@ if __name__ == "__main__":
     print(pic.size)
     pic_matrix = np.array(pic).reshape((height, width))
 
-    
-    lineart_matrix = np.zeros(shape=(height - 2, width - 2), dtype=np.int8)
-    convolve(pic_matrix, lineart_matrix)
+    lineart_matrix = convolve(pic_matrix,)
     lineart = Image.fromarray(lineart_matrix, "L")
     lineart.save(Path(args.output_folder_path, pic_name))
 
